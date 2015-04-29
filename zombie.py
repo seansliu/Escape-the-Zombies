0b479f8eacd89243ce87c6dd57f3f1a89e7be6a5
sophielucy <sophiejlucy@gmail.com>
2015-04-29 12:29:01 -0400

    Update zombie.py
    sophie's 1st stab at the functions

--- zombie.py
+++ zombie.py
@@ -1,15 +1,43 @@
 # Escape the Zombie backend functions
 #
-# Michelle Lee, Sean Liu, Sophy Lucy
+# Michelle Lee, Sean Liu, Sophie Lucy
 
-def index_dict():
-    return
+import random
 
-def generate_word(word_bank, length):
-    return
+class zomb:
+    
+    def __init__(self):
+        self.dictionary = dict()
+        
+    #make new dict with all words in dictionary
+    def index_dict(self):
+        
+        dictfile = open('/usr/share/dict/words','r')
+        dictlist = dictfile.readlines()
+        
+        for line in dictlist:
+            word = line.strip()
+            wlen = len(word)
+            
+            if wlen not in self.dictionary:
+                self.dictionary[wlen] = [word]
+                print wlen, word
+                
+            else:
+                self.dictionary[wlen].append(word) 
 
-def check_letter(word, letter):
-    return
-
+        return self.dictionary
+                
+    def generate_word(self):
+        #generate random number
+        num = random.randint(2,23)
+        print num        
+        #generate random word from number
+        ranword = random.choice(self.dictionary[num])
+        print ranword
+        
+        return ranword
+        
+    
 # others...
 
